974df17d2f4053f40b3aedc264da64668d193dcd
juanlumn <juanlumn@gmail.com>
2018-07-09 20:28:02 +0200

    Updated CORS

--- src/main/java/com/juanlumn/questionscrud/config/CROSConfig.java
+++ src/main/java/com/juanlumn/questionscrud/config/CROSConfig.java
@@ -15,7 +15,8 @@ public class CROSConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
-                    .allowedOrigins("http://localhost:3000", "http://localhost:5000");
+                    .allowedOrigins("http://localhost:3000", "http://localhost:5000", "http://192.168.1.138:3000",
+                        "http://192.168.1.138:5000");
             }
         };
     }
