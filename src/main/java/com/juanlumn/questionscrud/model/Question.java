23adaef09517f6b4f3694ddeefa3239e6139c1d2
juanlumn <juanlumn@gmail.com>
2018-08-15 21:19:37 +0200

    Updated CRUD

--- src/main/java/com/juanlumn/questionscrud/model/Question.java
+++ src/main/java/com/juanlumn/questionscrud/model/Question.java
@@ -36,7 +36,7 @@ public class Question implements Serializable {
     /**
      * {@link String} with the question itself
      */
-    @Column
+    @Column(columnDefinition="text")
     String question;
 
     /**
